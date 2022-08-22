68ba04fe4a89ef69d74c92c9f88296f60a460dc9
ba_c <swunhtetoo@gmail.com>
2022-08-22 15:18:52 +0630

    adding authentication

---
+++ resources/views/forms/login.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.baseLayout')
+@section('loginForm')
+    <form class="w-1/3 m-auto mt-20" method="POST" action="/authLogin">
+
+        @csrf
+
+        <div class="mb-6">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
+            <input type="text" id="name" name="name"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-300 focus:border-orange-300 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-300 dark:focus:border-orange-300"
+                placeholder="morgan" required>
+        </div>
+        <div class="mb-6">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+            <input type="text" id="email" name="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-300 focus:border-orange-300 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-300 dark:focus:border-orange-300"
+                placeholder="morgan" required>
+        </div>
+        <div class="mb-6">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
+            <input type="text" id="password" name="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-300 focus:border-orange-300 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-300 dark:focus:border-orange-300"
+                placeholder="morgan" required>
+        </div>
+
+        <div class="w-full flex justify-end">
+            <button type="submit"
+                class="cursor-pointer text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2 mb-6">
+                Submit
+            </button>
+        </div>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,17 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DetailsController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\FinancesController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MessagesController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\StockController;
+use Egulias\EmailValidator\Result\Reason\DetailedReason;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+
+Auth::routes();
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,29 +28,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index']);
-Route::get('/details', [DetailsController::class, 'index']);
+Route::group(['middleware' => ['auth']], function () {
 
-Route::get('/services', [ServicesController::class, 'index']);
-Route::get('/employees', [EmployeeController::class, 'index']);
-Route::get('/stock', [StockController::class, 'index']);
-Route::get('/messages', [MessagesController::class, 'index']);
-Route::get('/finances', [FinancesController::class, 'index']);
+    Route::get('/', [DashboardController::class, 'index']);
+    Route::get('/details', [DetailsController::class, 'index']);
+    
+    Route::get('/services', [ServicesController::class, 'index']);
+    Route::get('/employees', [EmployeeController::class, 'index']);
+    Route::get('/stock', [StockController::class, 'index']);
+    Route::get('/messages', [MessagesController::class, 'index']);
+    Route::get('/finances', [FinancesController::class, 'index']);
+    
+    
+    Route::get('/settings', function() {
+    });
+    
+    Route::post('/details/add', [DetailsController::class, 'store']);
+    
+    Route::post('/details/edit', [DetailsController::class, 'edit']);
+    
+    Route::get('/details/destroy/{id}', [DetailsController::class, 'destroy']);
+    
+    Route::post('/employees/add', [EmployeeController::class, 'store']);
+    
+    Route::get('/employees/edit/{id}', [EmployeeController::class, 'edit']);
+    
+    Route::post('/employees/update/{id}', [EmployeeController::class, 'update']);
+    
+    Route::get('/employees/destroy/{id}', [EmployeeController::class, 'destroy']);
 
-
-Route::get('/settings', function() {
 });
 
-Route::post('/details/add', [DetailsController::class, 'store']);
 
-Route::post('/details/edit', [DetailsController::class, 'edit']);
+Route::post('/authLogin', [LoginController::class, 'signin']);
+Route::get('login', [LoginController::class, 'show'])->name('login');
 
-Route::get('/details/destroy/{id}', [DetailsController::class, 'destroy']);
 
-Route::post('/employees/add', [EmployeeController::class, 'store']);
+Auth::routes();
 
-Route::get('/employees/edit/{id}', [EmployeeController::class, 'edit']);
-
-Route::post('/employees/update/{id}', [EmployeeController::class, 'update']);
-
-Route::get('/employees/destroy/{id}', [EmployeeController::class, 'destroy']);
+Route::get('/home', [DashboardController::class, 'index'])->name('home');
